86a3ed79cd5a964feb95907c614648a43405ada5
panjd123 <1747366367@qq.com>
2022-10-09 23:03:18 +0800

    整合了金额和刑期

--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 # Author: Bao Jx
+from calendar import month
 import re
 
 
-def string_to_yuan(string, base_unit='y', right_side=False):
+def string_to_yuan(string, base_unit='y', right_side=False,normalize = True):
     """
     Convert the traditional expression of price to the numerical
     value in the measurement unit of yuan.
@@ -25,6 +26,9 @@ def string_to_yuan(string, base_unit='y', right_side=False):
     :param right_side:
     :return: numerical data
     """
+    
+    string = re.sub('两', '二', string)
+    
     def normal(string):
         string = re.sub('億', '亿', string)
         string = re.sub('萬', '万', string)
@@ -42,6 +46,11 @@ def string_to_yuan(string, base_unit='y', right_side=False):
         string = re.sub('捌', '八', string)
         string = re.sub('玖', '九', string)
         string = re.sub('元|圓|圆', '', string)
+        
+        for i,n1 in enumerate(['一', '二','两', '三', '四', '五', '六', '七', '八', '九']):
+            for j,n2 in enumerate(['一', '二','两', '三', '四', '五', '六', '七', '八', '九']):
+                string = re.sub(n1 + n2, str(round((i+j)/2,1)), string)
+                
         return string
 
     UNIT_SEQ = 'gsbqwy'
@@ -51,7 +60,7 @@ def string_to_yuan(string, base_unit='y', right_side=False):
     SINGLE_CHAR = {'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9, '十': 10 ** 1, '百': 10 ** 2,
                    '千': 10 ** 3, '万': 10 ** 4, '亿': 10 ** 8}
 
-    if base_unit == 'y':
+    if base_unit == 'y' and normalize:
         string = normal(string)
 
     if re.match('^\d+(\.\d*)?$', string):
@@ -77,4 +86,22 @@ def string_to_yuan(string, base_unit='y', right_side=False):
             + string_to_yuan(odd, UNIT_SEQ[UNIT_SEQ.index(base_unit) - 1], right_side=True)
 
 
-print(string_to_yuan('三千元'))
+
+def string_to_month(string:str)->int:
+    has_year = string.find('年') != -1
+    has_month = string.find('月') != -1
+    string = re.sub('个', '', string)
+    string = re.sub('零', '', string)
+    year_month = re.split("[年月]", string)
+    
+    
+    if has_year and has_month:
+        return string_to_yuan(year_month[0],normalize=False) * 12 + string_to_yuan(year_month[1],normalize=False)
+    elif has_year:
+        return string_to_yuan(year_month[0],normalize=False) * 12
+    else:
+        return string_to_yuan(year_month[0],normalize=False)
+        
+if __name__=='__main__':
+    print(string_to_yuan('三千元'))
+    print(string_to_month('十五年五个月'))
